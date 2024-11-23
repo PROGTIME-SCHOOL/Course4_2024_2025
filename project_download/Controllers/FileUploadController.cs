3f325424152cca9639c574cd60a9dbac5925751f
Ilya <viosagmir@gmail.com>
2024-11-23 13:46:40 +0300

    Added Page Downloads

--- project_download/Controllers/FileUploadController.cs
+++ project_download/Controllers/FileUploadController.cs
@@ -11,8 +11,32 @@ public class FileUploadController : Controller
         return View();
     }
 
+    [HttpGet]
+    public IActionResult Downloads()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var uploadsFolder = Path.Combine(currentDirectory, "UploadedFiles");
+
+        if (!Directory.Exists(uploadsFolder))
+        {
+            return Content("Error. Directory does not exist!");
+        }
+
+        var files = Directory.GetFiles(uploadsFolder).
+            Select(file => new FileInfo(file)).
+            Select(file => new {
+                Name = file.Name,
+                Size = $"{(file.Length / 1024f / 1024f):F2} МБ"
+            }).ToList();
+
+        return View(files);
+    }
+
     public IActionResult Upload(IFormFile file)
     {
+        // альтернативный вариант загрузки через HttpContext
+        // var files = HttpContext.Request.Form.Files.FirstOrDefault();
+
         if (file == null || file.Length == 0)
         {
             return Content("Файл не выбран!");
@@ -49,5 +73,20 @@ public class FileUploadController : Controller
 
         byte[] bytes = System.IO.File.ReadAllBytes(filePath);
         return File(bytes, "application/octet-stream", fileName);
+    }
+
+    public IActionResult DeleteFile(string fileName)
+    {
+        var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+
+        var filePath = Path.Combine(uploadsFolder,fileName);
+
+        if (System.IO.File.Exists(filePath))
+        {
+            System.IO.File.Delete(filePath);
+            return Content($"File <{fileName}> was deleted");
+        }
+
+        return Content($"IMPORTANT!!! File <{fileName}> was not deleted!!!");
     }
 }
